9273f9a0c85065e1fd1aeb1263591ae077a5b692
Malik AlNajjar <malik-naj@hotmail.com>
2026-06-27 17:14:20 +0300

    Inventory: per-brand flavour lists; table columns swap Category for Brand + Flavour

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 
-const CACHE = 'mmcq-v28';
+const CACHE = 'mmcq-v29';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=28',
+  './styles.css?v=29',
   './manifest.json',
   './logo.png',
   './brand.png',
