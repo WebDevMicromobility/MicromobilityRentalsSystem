33fae6bbc2742754d0039b54c0e0fec53a158521
Malik AlNajjar <malik-naj@hotmail.com>
2026-06-28 01:32:08 +0300

    Nutrition: fibre/salt + vitamins & minerals (NRV%) editor and label; one-tap Vitamin Well prefills (Reload/Recover/Hydrate/Antioxidant/Upgrade)

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 
-const CACHE = 'mmcq-v35';
+const CACHE = 'mmcq-v36';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=35',
+  './styles.css?v=36',
   './manifest.json',
   './logo.png',
   './brand.png',
