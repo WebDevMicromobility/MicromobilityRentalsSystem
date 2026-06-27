b741f2e5106d02375b349648325c5c76c73ae85a
Malik AlNajjar <malik-naj@hotmail.com>
2026-06-27 16:36:04 +0300

    Inventory: rename Electrolyte Sachets to Electrolytes; group Supplements & Equipment into per-category sub-sections

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 
-const CACHE = 'mmcq-v26';
+const CACHE = 'mmcq-v27';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=26',
+  './styles.css?v=27',
   './manifest.json',
   './logo.png',
   './brand.png',
