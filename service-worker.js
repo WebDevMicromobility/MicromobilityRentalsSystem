2174907be66df9019a34465aeda24f1c11b55ea0
Malik AlNajjar <malik-naj@hotmail.com>
2026-07-02 03:16:46 +0300

    Remove staff Dashboard tab

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 
-const CACHE = 'mmcq-v51';
+const CACHE = 'mmcq-v52';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=51',
+  './styles.css?v=52',
   './manifest.json',
   './logo.png',
   './brand.png',
