79e45d4d0690c07b7f9f41a27b1bcc55b4b6fdd2
Malik AlNajjar <malik-naj@hotmail.com>
2026-07-02 16:56:14 +0300

    Show chosen add-ons in the customer booking review summary

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 
-const CACHE = 'mmcq-v63';
+const CACHE = 'mmcq-v64';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=63',
+  './styles.css?v=64',
   './manifest.json',
   './logo.png',
   './brand.png',
