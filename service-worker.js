94300f26c12f854c6fe715dbf8cd08081c1911bf
Malik AlNajjar <malik-naj@hotmail.com>
2026-07-03 04:13:43 +0300

    Merge Logs into History as a sub-tab (Rides / Action Log)

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
 
-const CACHE = 'mmcq-v75';
+const CACHE = 'mmcq-v76';
 const SHELL = [
   './',
   './index.html',
-  './styles.css?v=75',
+  './styles.css?v=76',
   './manifest.json',
   './logo.png',
   './brand.png',
