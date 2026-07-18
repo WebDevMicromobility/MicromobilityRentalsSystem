7a024d1ca1fbf4b0633bf4ad8c1627dce3a0f118
Malik AlNajjar <malik-naj@hotmail.com>
2026-07-18 05:01:54 +0300

    Staff table: dash out price and payment cells for social-ride rows

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'mmcq-v168';
+const CACHE = 'mmcq-v169';
 const IMG_CACHE = 'mmcq-img'; // Supabase Storage photos; persists across app versions (content-addressed)
 const SHELL = [
   './',
