05ba24be66d18a8b45b2e2dcd3f5ced43b0785c3
Malik AlNajjar <malik-naj@hotmail.com>
2026-07-23 10:19:47 +0300

    Saturday Social Ride visible to all customers when open; seats still staff-approved

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'mmcq-v175';
+const CACHE = 'mmcq-v176';
 const IMG_CACHE = 'mmcq-img'; // Supabase Storage photos; persists across app versions (content-addressed)
 const SHELL = [
   './',
