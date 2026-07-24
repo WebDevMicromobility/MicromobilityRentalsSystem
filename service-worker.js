a87f5c0853df7c7b365af8ff97804a9d31ef966d
Malik AlNajjar <malik-naj@hotmail.com>
2026-07-24 19:35:48 +0300

    Add 'I have my own bike' type (staff sees Bike owner, price 0); Saturday roster print report (name/phone/height/type/status)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'mmcq-v190';
+const CACHE = 'mmcq-v191';
 const IMG_CACHE = 'mmcq-img'; // Supabase Storage photos; persists across app versions (content-addressed)
 const SHELL = [
   './',
